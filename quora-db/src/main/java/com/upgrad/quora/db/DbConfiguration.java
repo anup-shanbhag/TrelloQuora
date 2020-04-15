476a846269a289ee9b713fbe39c1145c899f41ad
Anup Shanbhag <anupshanbhag.sd11@iiitb.net>
2020-04-15 21:14:43 +0200

    Configured spring boot maven plugin and updated test data

--- quora-db/src/main/java/com/upgrad/quora/db/DbConfiguration.java
+++ quora-db/src/main/java/com/upgrad/quora/db/DbConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EntityScan("com.upgrad.quora.db.entity")
-@EnableJpaRepositories("com.upgrad.quora.db.dao")
+@EntityScan(basePackages = "com.upgrad.quora.db.entity")
+@EnableJpaRepositories(basePackages = "com.upgrad.quora.db.dao")
 public class DbConfiguration {
 }
